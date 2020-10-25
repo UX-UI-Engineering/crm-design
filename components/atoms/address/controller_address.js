56ccf56b3d644192571485aad19c5edcf3f81ad1
Abhinay Thakur <a8h1nay@gmail.com>
2020-10-25 13:48:37 +0530

    Defines a controller architecture
    Use a demo component. Use lodash to inherent from controller class. Each controller has a `template` and `load` methods. Purpose of `load` is to intialize the compiled markup into the HTML document.

---
+++ components/atoms/address/controller_address.js
@@ -0,0 +1,24 @@
+(() => {
+    // Inheriting AppController class
+    APP.controller.address = _.assignIn({}, new AppController);
+    
+    let address = APP.controller.address;
+    address.template = APP.view.address;
+    address.load = () => {
+        //let theTemplateScript = document.getElementById('hndl-address').innerHTML;
+
+        // Compile the template
+        let theTemplate = Handlebars.compile(APP.view.address);
+
+        // Define our data object
+        let context = {
+            "city": "London",
+            "street": "Baker Street",
+            "number": "221B"
+        };
+
+        // Pass our data to the template
+        let theCompiledHtml = theTemplate(context);
+        document.getElementById('content-placeholder').innerHTML = theCompiledHtml;
+    }
+})();
