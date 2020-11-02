44eef3fcf7614346b4b3dd927fef7f4ccd61aaf7
Abhinay Thakur <a8h1nay@gmail.com>
2020-11-02 20:41:23 +0530

    Create and style Table Actions component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ActionComponent } from './button/action/action.component';
-import { SidebarComponent } from './navigation/sidebar/sidebar.component';
-import { RightComponent } from './area/right/right.component';
-import { FilterComponent } from './table/filter/filter.component';
-import { ContainerComponent } from './container/container.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ActionComponent } from "./button/action/action.component";
+import { SidebarComponent } from "./navigation/sidebar/sidebar.component";
+import { RightComponent } from "./area/right/right.component";
+import { FilterComponent } from "./table/filter/filter.component";
+import { ContainerComponent } from "./container/container.component";
+import { TableActionsComponent } from "./table/table-actions/table-actions.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ActionComponent,
-    SidebarComponent,
-    RightComponent,
-    FilterComponent,
-    ContainerComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ActionComponent,
+        SidebarComponent,
+        RightComponent,
+        FilterComponent,
+        ContainerComponent,
+        TableActionsComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
